722146e0c79ff73214e1591c7c348f3816566364
Vixx <vincent.asta1@gmail.com>
2022-05-26 23:51:52 +0200

    update

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -13,4 +13,16 @@ final class HomeController extends Controller
         echo $this->twig->render('home.twig');
     }
     
+    public function likeAction()
+    {
+        $likesCount = 0 ;
+        if (!isset($_SESSION['like'])){
+            $_SESSION['like'] = true;
+            $likesCount = $likesCount + 1;
+            var_dump($likesCount);
+            die();
+        }
+        file_put_contents('like.txt', $likesCount);
+    }
+
 }
